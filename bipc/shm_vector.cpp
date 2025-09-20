3c1035ca8ebfafd075c756facd57fe797a4fb889
sirvegetable <17wgs@queensu.ca>
2025-09-20 13:08:40 -0400

    a vector class in shared memory

--- bipc/shm_vector.cpp
+++ bipc/shm_vector.cpp
@@ -1,7 +1,8 @@
 #include <boost/interprocess/managed_shared_memory.hpp> 
 #include <boost/interprocess/containers/vector.hpp> 
 #include <boost/interprocess/allocators/allocator.hpp> 
-
+#include <cstdlib> 
+#include <string> 
 using namespace boost::interprocess; 
 
 /* 
@@ -13,17 +14,48 @@ using ShmemAllocator = allocator<int, managed_shared_memory::segment_manager>;
 
 using MyVector = vector<int,ShmemAllocator>; 
 
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
     if(argc == 1)
     {
-        struct shm_remove
+        struct shm_remover
         {
             shm_remover(){ shared_memory_object::remove("MySharedMemory"); }
             ~shm_remover(){shared_memory_object::remove("MySharedMemory"); }
-        }remover;
+        } remover;
         
-        managed_shared_memory segment(create_only_t, "MySharedMemory", 65536); 
-        
+        managed_shared_memory segment(create_only, "MySharedMemory", 65536); 
+        const ShmemAllocator alloc(segment.get_segment_manager()); 
+
+        MyVector* vec = segment.construct<MyVector>("MyVector")(alloc); 
+        for(int i = 0; i < 100; i++)
+        {
+            vec->push_back(i); 
+        }
+
+        std::string s(argv[0]); 
+        s += " child"; 
+        if(0 != std::system(s.c_str()))
+        {
+            return 1; 
+        }
+
+        /* check if child process destroyed vector*/
+        if(segment.find<MyVector>("MyVector").first)
+        {
+            return 1; 
+        }
     }
+    else 
+    {
+        managed_shared_memory segment(open_only, "MySharedMemory"); 
+        
+        /* find the vector */
+        MyVector* shm_vec = segment.find<MyVector>("MyVector").first;
+        /* use vector in reverse order */
+        std::sort(shm_vec->rbegin(), shm_vec->rend()); 
+        segment.destroy<MyVector>("MyVector"); 
+    }
+
+    return 0; 
 }
